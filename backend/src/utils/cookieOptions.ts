b6717cd6557938ba3bf05680f93a85ddb7933c24
jaimansoni <jaimansoni@gmail.com>
2025-05-27 15:19:43 +0530

    Changed sameSite cookies option

--- backend/src/utils/cookieOptions.ts
+++ backend/src/utils/cookieOptions.ts
@@ -3,14 +3,13 @@ import { CookieOptions } from "express";
 export const refreshTokenCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict" as const,
+  sameSite: "none" as const,
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
-
 
 export const accessTokenCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: true,
-  sameSite: "strict" as const,
+  sameSite: "none" as const,
   maxAge: 60 * 60 * 1000,
 };
