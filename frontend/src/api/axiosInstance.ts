993021e4ddb5b1a7290f8da96f9fa78e46e11ed9
jaimansoni <jaimansoni@gmail.com>
2025-05-27 10:33:38 +0530

    Added vercel.json

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/v1",
+  baseURL: "https://jwt-auth-starter.onrender.com/v1",
+  // baseURL: "http://localhost:3001/v1",
   withCredentials: true,
 });
 
